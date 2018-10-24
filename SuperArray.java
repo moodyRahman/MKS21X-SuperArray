115ae2f926051ba0b1fbe2f05f30ecdb6da7beb2
Moody <mrahman01@stuy.edu>
2018-10-23 23:49:36 -0400

    revamped size; added set; added toString; added contains; added add

--- SuperArray.java
+++ SuperArray.java
@@ -24,8 +24,43 @@ public class SuperArray{
   }
 
   public int size(){
-    return this.arraystg.length;
+    int output = 0;
+    for (int x = 0; x < this.arraystg.length; x++){
+      if (arraystg[x] != null){
+        output++;
+      }
+    }
+    return output;
   }
 
-  
+  public String set(int index, String element){
+    String temp = this.arraystg[index];
+    this.arraystg[index] = element;
+    return temp;
+  }
+
+  public String toString(){
+    String output = "";
+    for (int x = 0; x < arraystg.length; x++){
+      output += arraystg[x];
+    }
+    return "[" + output + "]";
+  }
+
+  public boolean contains(String element){
+    for (int x = 0; x < this.arraystg.length; x++){
+      if (arraystg[x] == element) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean add(String element){
+    String[] output = new String[this.arraystg.length + 1];
+    output[output.length - 1] = element;
+    return true;
+  }
+
+
 }
