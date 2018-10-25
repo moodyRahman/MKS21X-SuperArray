4746f95e8ce0dfd0ff61e5a32b2befa5eb1e369d
Moody <mrahman01@stuy.edu>
2018-10-25 11:31:11 -0400

    added test cases in Driver for set()

--- Driver.java
+++ Driver.java
@@ -17,7 +17,7 @@ public class Driver{
     System.out.println("obj t and its size");
     System.out.println(t);
     System.out.println(t.size());
-    System.out.println("testing set and get");
+    System.out.println("testing get");
 
     System.out.print("at 0: ");
     System.out.println(t.get(0));
@@ -36,5 +36,33 @@ public class Driver{
 
     System.out.println(t.get(6));
     System.out.print("at 6: ");
+
+    System.out.println("");
+    System.out.println("");
+
+
+    System.out.println("testing set");
+    System.out.println(t);
+
+    System.out.println("adding trubbish at 0");
+    t.set(0, "trubbish");
+    System.out.println(t);
+    System.out.println("");
+
+
+    System.out.println("adding skitty at 5; should throw error");
+    t.set(5, "skitty");
+    System.out.println(t);
+    System.out.println("");
+
+    System.out.println("adding pikapika at 3");
+    t.set(3, "pikapika");
+    System.out.println(t);
+    System.out.println("");
+
+    System.out.println("adding maneky at 4");
+    t.set(4, "maneky");
+    System.out.println(t);
+    System.out.println("");
   }
 }
